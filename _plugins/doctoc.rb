b14cdeee9a8692983b4fb6fa05399d0a68cb0c71
nounch <nounch@outlook.com>
2014-01-09 21:58:19 -0500

    Replace naive nested list tree by object-based tree data structure
    - `Tree' is a tree of `TreeNode' objects.
    - A `TreeNode' object has a name which is a unique name which
      should be a *NIX path name. Attention: Uniqueness checking is
      neither done by the `TreeNode' or the `Tree', but is the
      responsibility of the user!
    - Both, `Tree' and `TreeNode' objects provide a `html' method which
      returns a HTML representation (nested `ul' lists) of either the
      whole tree or the tree node with its whole subtree.
    - The `Toc' module provides a `toc' directive which holds the
      current HTML representation of the tree.

--- _plugins/doctoc.rb
+++ _plugins/doctoc.rb
@@ -1,40 +1,123 @@
-require 'pp'
-
 module Toc
 
-  class PathTree
-    attr_accessor :root_node
 
-    def initialize
-      @root_node = PathNode.new
-      @root_node.name = 'root'
+  #========================================================================
+  # Path Tree
+  #========================================================================
+
+  class TreeNode
+    attr_accessor :name, :data, :children, :html_string
+
+    def initialize(name, data, children = [])
+      @name = name
+      @data = data
+      @children = children
+
+      @pp_indentation_width = 2
+      @html_string = []
     end
 
-    def insert(node=@root_node, path_elements)
-      # path_elements.each_with_index do |path_element, i|
-      #   node.children.each do |child|
-      #     if node.name == path_element
-      #       node.insert(node.children[path_element], path_element[i..-1])
-      #     else
-      #       node.children << path_elements[i..-1]
-      #     end
-      #   end
-      # end
+    def pp(counter, string)
+      string << ' ' * (counter * @pp_indentation_width) +
+        File.basename(@name) + "\n"
+      counter += 1
+
+      @children.each do |child|
+        child.pp counter, string
+      end
+
+      counter -= 1
+    end
+
+    def html
+      self.htmlize([]).join("\n")
+    end
+
+    def htmlize(html)
+      html << '<ul>'
+      html << "<li><a href=\"#{@name}\">#{File.basename @name}</a></li>"
+
+      @children.each do |child|
+        child.htmlize html
+      end
+
+      html << '</ul>'
     end
 
   end
 
 
-  class PathNode
-    attr_accessor :parent, :children, :name
+  class Tree
+    attr_accessor :root
 
-    def initialize
-      @name = nil
-      @parent = nil
-      @children = []
+    def initialize(root)
+      @root = root
+      @last_found_node = nil  # Semi-global state!
     end
+
+    def find(node_name, node, is_node=false)
+      # Special case: the searched node is one of the children of the root
+      # note.
+      if is_node
+        if node.children.collect { |c| c.name }.include? node_name
+          @last_found_node = node.children[0]
+        end
+      end
+
+      node.children.each do |child|
+        children = child.children.select { |n| n.name == node_name }
+
+        if (!children.collect { |n| n.name }.include? node_name)
+          self.find node_name, child
+        else
+          @last_found_node = children[0]  # Semi-global state!
+        end
+
+      end
+      return @last_found_node
+    end
+
+    def insert_child(node_name, new_node, node=@root)
+      node.children.each do |child|
+        if child.name == node_name
+          child.children << new_node
+        else
+          self.insert_child node_name, new_node, child
+        end
+
+      end
+    end
+
+    def insert_pathes(pathes)
+      pathes.each_pair do |key, value|
+        value.each_pair do |k, v|
+          if File.basename(k) != 'index.html'
+            self.insert_child(key, TreeNode.new(k, ['data']))
+          end
+        end
+
+        self.insert_pathes value
+      end
+    end
+
+    def pp
+      counter = 0
+      string = []
+      @root.pp counter, string
+      string.join
+    end
+
+    def html
+      @root.htmlize @root.html_string
+      @root.html_string.join "\n"
+    end
+
   end
 
+
+  #========================================================================
+  # Plugin
+  #========================================================================
 
   class Generator < Jekyll::Generator
 
@@ -46,47 +129,41 @@ module Toc
 
     def generate_tree(pathes)
       pathes.each do |path|
-        current  = @tree
+        current = @tree
+        counter = 0
+
         path.split("/").inject("") do |sub_path, dir|
           sub_path = File.join(sub_path, dir)
-          current[sub_path] ||= {}
+
+          if (current[sub_path] == nil || current[sub_path] == false) &&
+              sub_path != 'index.html'
+            current[sub_path] = { }
+          end
+
           current = current[sub_path]
+
           sub_path
         end
       end
     end
 
-    def generate_nested_list(prefix, node)
-      @nested_list += "#{prefix}<ul>"
-      node.each_pair do |path, subtree|
-        # @nested_list +=
-        #   "#{prefix}  <li>[#{path[1..-1]}] #{File.basename(path)}</li>"
-        @nested_list +=
-          # "#{prefix}  <li>[#{path[1..-1]}] #{File.basename(path)}</li>"
-          "<li>#{path.split('/')[-1]}</li>"
-        generate_nested_list(prefix + "  ", subtree) unless subtree.empty?
-      end
-      @nested_list += "#{prefix}</ul>"
-    end
-
     def generate(site)
-      # toc = PathTree.new()
-      # site.pages.each do |page|
-      #   page.data['toc'] = page.path.split('/')[1...-1]
-      #   toc.insert page.path.split('/')[1...-1]
-      # end
-      # pp toc
-
       page_pathes = site.pages.each.collect { |page| page.path }
 
       generate_tree(page_pathes)
 
-      pp @tree
+      path_tree = Tree.new(TreeNode.new('root', ['data'],
+                                        [TreeNode.new('/pages',
+                                                      ['data'])]))
 
-      toc = generate_nested_list '', @tree
+      path_tree.insert_pathes({'/pages' => @tree['/pages'] })
+
+      toc = path_tree.html
+
       site.pages.each do |page|
         page.data['toc'] = toc
       end
+
     end
   end
 
